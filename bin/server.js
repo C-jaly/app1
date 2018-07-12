ac42b01ccd2c88cc45e3a482f4e46200c252005a
陈建丽 <cjl@chenjianlideMacBook-Pro.local>
2018-07-12 11:23:56 +0800

    feat(babel): 添加对server下文件的babel转义

---
+++ bin/server.js
@@ -0,0 +1,20 @@
+require('../server.babel')
+const path = require('path')
+const rootDir = path.resolve(__dirname, '..') // node执行的目录
+
+/**
+ * Define isomorphic constants
+ */
+global.IS_CLIENT = false;
+global.IS_SERVER = true;
+global.DISABLE_SSR = true;
+global.IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+global.ROOTDIR = rootDir
+
+const WebpackIsomorphicTools  = require('webpack-isomorphic-tools');
+global.webpackIsomorphicTools = new WebpackIsomorphicTools(require('../webpack/webpack-isomorphic-tools'))
+.server(rootDir, function() {
+  require('../src/server')
+})
+
+
